fafe525160ab98d2f6669a9f03bd5e6ad0440e60
Bruna Fernandez Artioli <bfernan2@student.42sp.org.br>
2025-07-22 19:14:03 -0300

    feat: add strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: bfernan2 <bfernan2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:24:13 by bfernan2          #+#    #+#             */
-/*   Updated: 2025/07/22 18:32:25 by bfernan2         ###   ########.fr       */
+/*   Updated: 2025/07/22 19:11:31 by bfernan2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,24 @@ char	*ft_strrchr(const char *s, int c)
 	i = 0;
 	while (s[i])
 	{
-		if ((unsigned char)s[i] == (unsigned char)c)
-			return ((char *)&s[i]);
 		i++;
 	}
+	i++;
+	while (i-- > 0)
+	{
+		if ((unsigned char)s[i] == (unsigned char)c)
+		{
+			return ((char *)(s + i));
+		}
+	}
+	return ((char *)0);
 }
+// #include <string.h>
+// #include <stdio.h>
+// int main(void)
+// {
+// 	char str[]="aaaaabaa";
+// 	char c = 'b';
+// 	printf("%s\n", strrchr(str, c));
+// 	printf("%s\n", ft_strrchr(str, c));
+// }
