3df6ee6c3b6c39c476a97e7521b01335799d83f3
Bruna Artioli <brunaartioli.ba@gmail.com>
2025-07-24 20:00:48 -0300

    feat: add strnstr initial

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,9 +14,44 @@
 
 char	*strnstr(const char *big, const char *little, size_t len)
 {
-	size_t	little_t;
+	size_t	i;
+	size_t	j;
+	const char	*l;
+	const char	*b;
 	
-	if (!little[0])
-		return ((char *)big);
-		
+	i = 0;
+	j = 0;
+	l = (unsigned char *)little;
+	b = (unsigned char *)big;
+	while (len > 0)
+	{
+		if (!little[0])
+			return ((char *)big);
+		if (little[i] != big[j])
+		{
+			return (NULL);
+			i++;
+			j++;
+		}
+	}
+	}
+#include <string.h>
+#include <stdio.h>
+int main(void)
+{
+	char str[25] = "Good Morning sunshine";
+	char str1[] = "Morning";
+	char *ptrstr;
+
+	ptrstr = strnstr(str, str1);
+
+	if (ptrstr)
+	{
+		printf("\n%c\n", *ptrstr);
+
+	}else
+	{
+		printf("\nNull pointer");
+	}
+	return (0);
 }
